b4c43d1d8474d42856cd3304b69dd5dbe80a290d
Macmini2 <macmini2@Macmini2s-Mac-mini.local>
2019-02-07 13:51:19 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>(
+    <div>
+        Hello Krishna
+    </div>
+)
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
